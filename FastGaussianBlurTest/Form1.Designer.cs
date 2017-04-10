e50cc9ce7cfa5e1a159cb37b5c4c136c12cf8e7d
unknown <>
2017-04-10 18:06:32 +0800

    添加项目文件。

---
+++ FastGaussianBlurTest/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace FastGaussianBlurTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(31, 91);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(500, 500);
+            this.panel3.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(566, 91);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(500, 500);
+            this.panel4.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(365, 613);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 50);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1245, 701);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ FastGaussianBlurTest/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FastGaussianBlurTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics GF;
+        int iSize = 500;
+        int R = 3;
+        // source channel, target channel, width, height, radius
+        int[] gaussBlur(int[] scl, int w, int h, int r)
+        {
+            int[] tcl = new int[10];
+            int rs = (int)(r * 2.57);     // significant radius
+            for (int i = 0; i < h; i++)
+                for (int j = 0; j < w; j++)
+                {
+                    int val = 0, wsum = 0;
+                    for (int iy = i - rs; iy < i + rs + 1; iy++)
+                        for (int ix = j - rs; ix < j + rs + 1; ix++)
+                        {
+                            int x = Math.Min(w - 1, Math.Min(0, ix));
+                            int y = Math.Min(h - 1, Math.Min(0, iy));
+                            int dsq = (ix - j) * (ix - j) + (iy - i) * (iy - i);
+                            int wght = (int)(Math.Exp((-dsq / (2 * r * r))) / (Math.PI * 2 * r * r));
+                            val += scl[y * w + x] * wght; wsum += wght;
+                        }
+                    tcl[i * w + j] = (int)(val / wsum);
+                }
+            return tcl;
+        }
+        Color[,] myGaussBlur(Color[,] Input,int r)
+        {
+            int l1 = Input.GetLength(0);
+            int l2 = Input.GetLength(1);
+            Color[,] Output = new Color[l1, l2];
+
+            for (int x = 0; x < l1; x++)
+                for (int y = 0; y < l2; y++)
+                {
+                    int count = 0;
+                    Single RC = 0, GC = 0, BC = 0;
+                    for (int ix = x - r; ix < x + r+1; ix++)
+                    {
+                        for (int iy = y - r; iy < y + r+1; iy++)
+                        {
+                            if (ix < 0) ix = 0;
+                            if (iy < 0) iy = 0;
+                            if (ix >= l1) break;
+                            if (iy >= l2) break ;
+                            RC += (Single)(Input[ix, iy].R * (1.0- ((Single)(Math.Abs(ix-x)* Math.Abs(iy - y))/ (r*r))));
+                            GC += (Single)(Input[ix, iy].G * (1.0 - ((Single)(Math.Abs(ix - x) * Math.Abs(iy - y)) / (r * r))));
+                            BC += (Single)(Input[ix, iy].B * (1.0 - ((Single)(Math.Abs(ix - x) * Math.Abs(iy - y)) / (r * r))));
+                            count++;
+                        }
+                    }
+                   Output[x, y] = Color.FromArgb(255, (int)RC / 90, (int)GC / 90, (int)BC / 90);
+
+                }
+            return Output;
+        }
+
+        int ImgDrawBefore(Color[,] Msgs)
+        {
+            panel3.Refresh();
+            GF = panel3.CreateGraphics();
+            label2.Text = "正在渲染原图";
+            label2.Refresh();
+            for (int i = 0; i < iSize; i++)
+            {
+                for (int ii = 0; ii < iSize; ii++)
+                {
+                    GF.DrawRectangle(new Pen(Msgs[i, ii]), new Rectangle(i, ii, 1, 1));
+                }
+                label2.Text = "正在渲染原图......" + (Single)i/iSize * 100 + "%";
+                label2.Refresh();
+            }
+            return 1;
+            
+        }
+        int ImgDrawAfter(Color[,] Msgs)
+        {
+            panel4.Refresh();
+            GF = panel4.CreateGraphics();
+            label2.Text = "正在渲染处理后的图像";
+            label2.Refresh();
+            for (int i = 0; i < iSize; i++)
+            {
+                for (int ii = 0; ii < iSize; ii++)
+                {
+                    GF.DrawRectangle(new Pen(Msgs[i, ii]), new Rectangle(i, ii, 1, 1));
+                }
+                label2.Text = "正在渲染处理后的图像......" + (Single)i / iSize * 100 + "%";
+                label2.Refresh();
+            }
+            return 1;
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            /*
+int[] scl = new int[10];
+for (int i = 0; i < scl.Length; i++)
+{
+    scl[i] = i + 100;
+}
+int[] tcl = gaussBlur(scl, 10, 10, 1);
+scl[1] = 1 + 100;
+
+*/
+            label3.Text = "类高斯模糊算法测试         模糊半径:"+ R +"                     点击开始          ";
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            int count = 50;
+            Image T1 = Bitmap.FromFile("D:\\1.png");
+            Bitmap TT = new Bitmap(T1);
+            Random Ran = new Random();
+            Color[,] Test = new Color[iSize, iSize];
+            Color[,] TestO = new Color[Test.GetLength(0), Test.GetLength(1)];
+            for (int i = 0; i < Test.GetLength(0); i++)
+                for (int ii = 0; ii < Test.GetLength(1); ii++)
+                {
+                    //TestO[i, ii] = Color.FromArgb(Ran.Next(1, 254), Ran.Next(1, 254), Ran.Next(1, 254));
+                    Test[i, ii] = TT.GetPixel(i, ii);
+                    //Test[i, ii] = Color.FromArgb(count+= +Ran.Next(1, 50), count+ Ran.Next(1, 50), count + Ran.Next(1, 50));
+                    if (count >= 154) count = 1;
+                }
+            
+
+            DateTime DTS = DateTime.Now;
+            DTS = DateTime.Now;
+            TestO = myGaussBlur(Test, 5);
+            //label3.Text += "   算法耗时："+ (DateTime.Now-DTS).ToString();
+            label3.Refresh();
+            ImgDrawBefore(Test);
+            ImgDrawAfter(TestO);
+            label2.Text = "渲染完成";
+            label2.Refresh();
+        }
+    }
+}
