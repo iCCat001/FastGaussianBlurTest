fcf0eeb77743b023282afaed34f604e06458d7c2
王凯 <ic-cat@outlook.com>
2017-04-13 16:37:04 +0800

    ？

--- FastGaussianBlurTest/Form1.cs
+++ FastGaussianBlurTest/Form1.cs
@@ -20,6 +20,8 @@ namespace FastGaussianBlurTest
         Graphics GF;
         int iSize = 500;
         int R = 7;
+        double MaxText = 160;
+        int MM=200;
         PrivateBitmap Source = new PrivateBitmap("D:\\1.png");
         // source channel, target channel, width, height, radius
         int[] gaussBlur(int[] scl, int w, int h, int r)
@@ -65,15 +67,78 @@ namespace FastGaussianBlurTest
                             RC += (Single)(Input[ix, iy].R * (1.0- ((Single)(Math.Abs(ix-x)* Math.Abs(iy - y))/ (r*r))));
                             GC += (Single)(Input[ix, iy].G * (1.0 - ((Single)(Math.Abs(ix - x) * Math.Abs(iy - y)) / (r * r))));
                             BC += (Single)(Input[ix, iy].B * (1.0 - ((Single)(Math.Abs(ix - x) * Math.Abs(iy - y)) / (r * r))));
+
+                            double t = RC > GC ? RC : GC;
+                            t = t > BC ? t : BC;
+                            MaxText = MaxText > t ? MaxText : t;
+
                             count++;
                         }
                     }
-                   Output[x, y] = Color.FromArgb(255, (int)RC / 200, (int)GC / 200, (int)BC / 200);
+                   Output[x, y] = Color.FromArgb(255, (int)RC / MM, (int)GC / MM, (int)BC / MM);
 
                 }
+            MaxText = MaxText / 255;
+            MM = (int)MaxText+1;
             return Output;
         }
+        Color[,] myGaussBlur1(Color[,] Input,int r)
+        {
+            int l1 = Input.GetLength(0);
+            int l2 = Input.GetLength(1);
+            Color[,] Output = new Color[l1, l2];
 
+            //正四棱锥模型（长=宽=高），模拟单位面积下使用积分，对高进行求值，用于分权
+            double Height = r; //正四棱锥
+            double S = r * r;
+            double Volume = (r * r * r) / 3; //体积
+            int[] Levels = new int[r];
+
+            Levels[0] = 1;
+            for(int i=1;i<r;i++)
+            {
+                Levels[i] = i * 4 + 4;  //每层单位数
+            }
+            double[] Heights = new double[r+1];
+            for(int i=0;i<r;i++)
+            {
+                Heights[i] = (((((i+1)*(i+1)+i*i)+(Height/r)
+                    + Math.Pow(((i + 1.0) * (i + 1.0) + i * i) * (Height / r),0.5)) /3)
+                    - (i*i* (Height/r)))/Levels[i] + (Height / r) * (r-i-1); //每层高度（近似）（权值）
+            }
+            //Heights[r] = 0;
+            double V=0;
+            for (int i = 0; i < r; i++)
+            {
+                V += Heights[i] * Levels[i];
+            }
+
+            for (int x = 0; x < l1; x++)
+                for (int y = 0; y < l2; y++)
+                {
+                    int count = 0;
+                    double RC = 0, GC = 0, BC = 0;
+                    for (int ix = x - r; ix < x + r + 1; ix++)
+                    {
+                        for (int iy = y - r; iy < y + r + 1; iy++)
+                        {
+                            if (ix < 0) ix = 0;
+                            if (iy < 0) iy = 0;
+                            if (ix >= l1) break;
+                            if (iy >= l2) break;
+                            int Floor = ((Math.Abs(ix - x)) >= (Math.Abs(iy - y)) ? Math.Abs(ix - x) : Math.Abs(iy - y));
+                            RC += ((Input[ix, iy].R) * Heights[r-Floor]);
+                            GC += ((Input[ix, iy].G) * Heights[r-Floor]);
+                            BC += ((Input[ix, iy].B) * Heights[r-Floor]);
+                            count++;
+                        }
+                    }
+                    Output[x, y] = Color.FromArgb(255, (int)(RC / (V)), (int)(GC / (V)), (int)(BC / (V)));
+
+                }
+
+            return Output;
+        }
         int ImgDrawBefore(Color[,] Msgs)
         {
             panel3.Refresh();
@@ -157,7 +222,11 @@ scl[1] = 1 + 100;
             DateTime DTS2;
             DateTime DTS = DateTime.Now;
             DTS = DateTime.Now;
+
+            //TestO = myGaussBlur1(Test, R);
+
             TestO = myGaussBlur(Test, R);
+
             DTS2 = DateTime.Now;
             label2.Text = "算法耗时："+ (DTS2-DTS).TotalSeconds.ToString()+ "(秒/s)";
             for (int i = 0; i < TestO.GetLength(0); i++)
